0e38dd0c6a4345b5e52aca523479655635005e6a
IgorApSilva <igoraparecido59@outlook.com>
2022-05-15 11:37:05 -0300

    add Tribonacci kata

---
+++ CodeWars.Kata.Tests/TribonacciSequence/XbonacciTests.cs
@@ -0,0 +1,28 @@
+﻿using CodeWars.Kata.TribonacciSequence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeWars.Kata.Tests.TribonacciSequence
+{
+    public class XbonacciTests
+    {
+        [Theory]
+        [InlineData(new double[] { 1, 1, 1 }, 10, new double[] { 1, 1, 1, 3, 5, 9, 17, 31, 57, 105 })]
+        [InlineData(new double[] { 0, 0, 1 }, 10, new double[] { 0, 0, 1, 1, 2, 4, 7, 13, 24, 44 })]
+        public void Xbonacci_CaculateTribonacci_WithSuccess(double[] input,int numberOfElements, double[] expected)
+        {
+            //Arrange
+            var xbonacci = new Xbonacci();
+
+            //Act
+            double[] actual = xbonacci.CaculateTribonacci(input, numberOfElements);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CodeWars.Kata/TribonacciSequence/Xbonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.TribonacciSequence
+{
+    public class Xbonacci
+    {
+        public double[] CaculateTribonacci(double[] input, int numberOfElements)
+        {
+            var result = new List<double>(input);
+
+            for(var x = result.Count; x<numberOfElements; x++)
+            {
+                var value = result[x-1] + result[x - 2] + result[x - 3];
+                result.Add(value);
+            }
+
+            return result.ToArray();
+        }
+
+    }
+}
