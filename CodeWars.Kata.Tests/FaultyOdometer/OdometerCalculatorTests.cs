3075bea0b1c559cc3d88f18f2f251aab582bbb77
IgorApSilva <igoraparecido59@outlook.com>
2022-05-15 10:55:09 -0300

    add faulty odometer kata

---
+++ CodeWars.Kata.Tests/FaultyOdometer/OdometerCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using CodeWars.Kata.FaultyOdometer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeWars.Kata.Tests.FaultyOdometer
+{
+    public class OdometerCalculatorTests
+    {
+        [Theory]
+        [InlineData(13,12)]
+        [InlineData(15,13)]
+        [InlineData(2003, 1461)]
+        [InlineData(2005, 1462)]
+        [InlineData(1500, 1053)]
+        [InlineData(55, 40)]
+        public void OdometerCalculator_CalculateOdometerRealDistance_WithSuccess(int currentMiles,int expected)
+        {
+            //Arrange
+            var odometerCalculator = new OdometerCalculator();
+
+            //Act
+            int actual = odometerCalculator.CalculateOdometerRealDistance(currentMiles);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void OdometerCalculator_CalculateOdometerRealDistance_InvalidMiles()
+        {
+            //Arrange
+            var odometerCalculator = new OdometerCalculator();
+            int currentMiles = 0;
+
+            //Act & //Assert
+            Assert.Throws<ArgumentException>(() => odometerCalculator.CalculateOdometerRealDistance(currentMiles));
+        }
+    }
+}
---
+++ CodeWars.Kata/FaultyOdometer/OdometerCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.FaultyOdometer
+{
+    public class OdometerCalculator
+    {
+        public int CalculateOdometerRealDistance(int currentMiles)
+        {
+            if (currentMiles < 1)
+                throw new ArgumentException(null, nameof(currentMiles));
+
+            var realCurrentMiles = 0;
+
+            for(var x = 0; x < currentMiles; x++)
+            {
+                if (!x.ToString().Contains('4'))
+                    realCurrentMiles++;
+
+            }
+
+            return realCurrentMiles;
+        }
+    }
+}
